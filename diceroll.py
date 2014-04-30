f8c0104b369f05a2f69c584759ce75b55274de87
Eric Yu <yuhongzeusa@gmail.com>
2014-04-30 21:58:28 +0800

    new changes again

--- diceroll.py
+++ diceroll.py
@@ -1,5 +1,8 @@
 import random
 import specialinput.py
+chances = int_input("how many dices do you want to roll?")
 min = 1
-max = int_input("how many sides should the dice have?")
-print "the result of the dice rolling is," random.randint(min,max)
+max = int_input("how many sides should each dice have?")
+while chaces != 0:
+    print "the result of the dice roll is", random.randint(min, max)
+
